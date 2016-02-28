b08fb7ca6aafc359eef056c3d00b6bb339db73b3
Alexander Zhang <darkpro2k13@gmail.com>
2016-02-28 14:29:47 -0500

    tentative email system
    send capability should work

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -1,5 +1,15 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**F
  * 
@@ -44,6 +54,43 @@ public class CommandLine {
 		} catch (InterruptedException errr) {
 			System.out.println("InterruptedException oh noes probably at line 51");
 		}
+	}
+	
+	public void sendEmail(String title, String body, String sender, String password, String receiver) {
+	      String to = receiver;
+
+	      String from = sender;
+
+	      String host = "localhost";
+
+	      Properties properties = System.getProperties();
+
+	      properties.setProperty("mail.smtp.host", host);
+
+	      Session session = Session.getDefaultInstance(properties);
+
+	      try{
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+	         // Set Subject: header field
+	         message.setSubject(title);
+
+	         // Now set the actual message
+	         message.setText(body);
+
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
 	}
 
 }

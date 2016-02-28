d3408a18f8d49a2441859b249e551cea141b75b0
Kevin Zhan <kevin.w.zhan@gmail.com>
2016-02-28 15:19:43 -0500

    Updated Display

--- src/DisplayGenerated.java
+++ src/DisplayGenerated.java
@@ -21,15 +21,19 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class DisplayGenerated extends JFrame implements ActionListener, ChangeListener {
-	private JTextField txtPhoneNumber;
-	private JTextField txtStatusHere;
-	private JTextField txtEmail;
-	private JCheckBox chckbxMute = new JCheckBox("Mute All");
-	private JButton btnTestMessage = new JButton("Test Message");
-	private JCheckBox chckbxSendToPhone = new JCheckBox("Send to Phone");
-	private JCheckBox chckbxSendToEmail = new JCheckBox("Send to Email");
-	private JCheckBoxMenuItem chckbxmntmMinimizeOnClose = new JCheckBoxMenuItem("Minimize on Close");
-	private JMenuItem mntmQuit = new JMenuItem("Quit");
+	private JTextField phoneField;
+	private JTextField statusField;
+//	private JTextField emailField;
+	public JCheckBox muteBox = new JCheckBox("Mute All");
+	public JButton test = new JButton("Test Message");
+	public JCheckBox phoneBox = new JCheckBox("Send to Phone");
+	public JCheckBox emailBox = new JCheckBox("Send to Email");
+	private JMenuItem quit = new JMenuItem("Quit");
+	public String status = "Okay";
+	public int interval = 1;
+//	private String email = "Insert Email here";
+	public String number = "Insert Number here";
+	public boolean muted = false, sendPhone = false, sendEmail = false;
 
 	/**
 	 * Create the frame.
@@ -39,7 +43,7 @@ public class DisplayGenerated extends JFrame implements ActionListener, ChangeLi
 	 * @throws ClassNotFoundException 
 	 */
 	public DisplayGenerated() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout(0, 0));
@@ -47,58 +51,61 @@ public class DisplayGenerated extends JFrame implements ActionListener, ChangeLi
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.SOUTH);
 
-		JLabel lblEmailAddress = new JLabel("Email Address:");
-		panel.add(lblEmailAddress);
+//		JLabel lblEmailAddress = new JLabel("Email Address:");
+//		panel.add(lblEmailAddress);
 
-		txtEmail = new JTextField();
-		txtEmail.setText("Email");
-		panel.add(txtEmail);
-		txtEmail.setColumns(10);
+//		emailField = new JTextField();
+//		emailField.setText(email);
+//		panel.add(emailField);
+//		emailField.setColumns(10);
 
 		JLabel lblPhoneNumber = new JLabel("Phone Number:");
 		panel.add(lblPhoneNumber);
 
-		txtPhoneNumber = new JTextField();
-		txtPhoneNumber.setText("Phone Number");
-		panel.add(txtPhoneNumber);
-		txtPhoneNumber.setColumns(10);
+		phoneField = new JTextField();
+		phoneField.setText(number);
+		panel.add(phoneField);
+		phoneField.setColumns(15);
 
 		JPanel panel_1 = new JPanel();
 		getContentPane().add(panel_1, BorderLayout.CENTER);
 		SpringLayout sl_panel_1 = new SpringLayout();
-		sl_panel_1.putConstraint(SpringLayout.EAST, btnTestMessage, -33, SpringLayout.WEST, chckbxSendToEmail);
-		sl_panel_1.putConstraint(SpringLayout.WEST, chckbxMute, 239, SpringLayout.WEST, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.NORTH, chckbxSendToEmail, 115, SpringLayout.NORTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, chckbxSendToEmail, -45, SpringLayout.SOUTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, chckbxMute, -6, SpringLayout.NORTH, chckbxSendToPhone);
-		sl_panel_1.putConstraint(SpringLayout.NORTH, chckbxSendToPhone, 89, SpringLayout.NORTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.WEST, chckbxSendToPhone, 0, SpringLayout.WEST, chckbxMute);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, chckbxSendToPhone, -6, SpringLayout.NORTH, chckbxSendToEmail);
-		sl_panel_1.putConstraint(SpringLayout.WEST, chckbxSendToEmail, 0, SpringLayout.WEST, chckbxMute);
+		sl_panel_1.putConstraint(SpringLayout.EAST, test, -33, SpringLayout.WEST, emailBox);
+		sl_panel_1.putConstraint(SpringLayout.WEST, muteBox, 239, SpringLayout.WEST, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, emailBox, 115, SpringLayout.NORTH, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.SOUTH, emailBox, -45, SpringLayout.SOUTH, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.SOUTH, muteBox, -6, SpringLayout.NORTH, phoneBox);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, phoneBox, 89, SpringLayout.NORTH, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, phoneBox, 0, SpringLayout.WEST, muteBox);
+		sl_panel_1.putConstraint(SpringLayout.SOUTH, phoneBox, -6, SpringLayout.NORTH, emailBox);
+		sl_panel_1.putConstraint(SpringLayout.WEST, emailBox, 0, SpringLayout.WEST, muteBox);
 		panel_1.setLayout(sl_panel_1);
-
-		btnTestMessage.addActionListener(this);
-		btnTestMessage.setActionCommand("test");
-		panel_1.add(btnTestMessage);
-		chckbxMute.addActionListener(this);
-		chckbxMute.setActionCommand("mute");
-		panel_1.add(chckbxMute);
-		chckbxSendToPhone.addActionListener(this);
-		chckbxSendToPhone.setActionCommand("phone");
-		panel_1.add(chckbxSendToPhone);
-		chckbxSendToEmail.addActionListener(this);
-		chckbxSendToEmail.setActionCommand("email");
-		panel_1.add(chckbxSendToEmail);
+		
+		test.addActionListener(this);
+		test.setActionCommand("test");
+		panel_1.add(test);
+		muteBox.addActionListener(this);
+		muteBox.setSelected(muted);
+		muteBox.setActionCommand("mute");
+		panel_1.add(muteBox);
+//		phoneBox.addActionListener(this);
+//		phoneBox.setSelected(sendPhone);
+//		phoneBox.setActionCommand("phone");
+//		panel_1.add(phoneBox);
+//		emailBox.addActionListener(this);
+//		emailBox.setSelected(sendPhone);
+//		emailBox.setActionCommand("email");
+//		panel_1.add(emailBox);
 		
 		JButton btnOpenLog = new JButton("Open Log");
-		sl_panel_1.putConstraint(SpringLayout.NORTH, btnTestMessage, 17, SpringLayout.SOUTH, btnOpenLog);
-		sl_panel_1.putConstraint(SpringLayout.WEST, btnTestMessage, 0, SpringLayout.WEST, btnOpenLog);
-		sl_panel_1.putConstraint(SpringLayout.NORTH, btnOpenLog, 0, SpringLayout.NORTH, chckbxMute);
-		sl_panel_1.putConstraint(SpringLayout.EAST, btnOpenLog, -29, SpringLayout.WEST, chckbxMute);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, test, 17, SpringLayout.SOUTH, btnOpenLog);
+		sl_panel_1.putConstraint(SpringLayout.WEST, test, 0, SpringLayout.WEST, btnOpenLog);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, btnOpenLog, 0, SpringLayout.NORTH, muteBox);
+		sl_panel_1.putConstraint(SpringLayout.EAST, btnOpenLog, -29, SpringLayout.WEST, muteBox);
 		panel_1.add(btnOpenLog);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(1, 0, 60, 1));
+		spinner.setModel(new SpinnerNumberModel(interval, 0, 60, 1));
 		panel_1.add(spinner);
 		
 		JLabel lblMessageInterval = new JLabel("Minutes to Mute After Alert");
@@ -115,11 +122,11 @@ public class DisplayGenerated extends JFrame implements ActionListener, ChangeLi
 		JLabel lblStatus = new JLabel("Status:");
 		panel_2.add(lblStatus);
 
-		txtStatusHere = new JTextField();
-		txtStatusHere.setEditable(false);
-		txtStatusHere.setText("Status Here");
-		panel_2.add(txtStatusHere);
-		txtStatusHere.setColumns(10);
+		statusField = new JTextField();
+		statusField.setEditable(false);
+		statusField.setText(status);
+		panel_2.add(statusField);
+		statusField.setColumns(10);
 
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -127,13 +134,9 @@ public class DisplayGenerated extends JFrame implements ActionListener, ChangeLi
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 
-		chckbxmntmMinimizeOnClose.addActionListener(this);
-		chckbxmntmMinimizeOnClose.setActionCommand("minimize");
-		mnFile.add(chckbxmntmMinimizeOnClose);
-
-		mntmQuit.addActionListener(this);
-		mntmQuit.setActionCommand("quit");
-		mnFile.add(mntmQuit);
+		quit.addActionListener(this);
+		quit.setActionCommand("quit");
+		mnFile.add(quit);
 	}
 
 	@Override

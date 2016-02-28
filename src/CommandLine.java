64a13b9d223a01b5041d34ef2adc844bb216ac0a
Alexander Zhang <darkpro2k13@gmail.com>
2016-02-28 09:57:08 -0500

    Whoo internet examplesclass SyncPipe implements Runnable

---
+++ src/CommandLine.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Gordon
+ *
+ */
+public class CommandLine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

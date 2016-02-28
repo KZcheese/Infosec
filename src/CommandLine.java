bdec7b7238822c028b57d66eb9eb8be35613191f
Richard <richard1248c@gmail.com>
2016-02-28 13:41:45 -0500

    mostest recentest command line thing

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -10,35 +10,40 @@ import java.io.PrintWriter;
  *
  */
 public class CommandLine {
-	
+
 	/**
 	 * @param args
 	 */
-	public CommandLine() {
-		String number = "9783936245";//change this number later
-		String[] command =
-		    {
-		        "cmd",
-		    };
-		    try {
+	public CommandLine(String task) {
+		String number = "9783936245";// change this number later
+		String[] command = { "cmd", };
+		try {
 			Process p = Runtime.getRuntime().exec(command);
-		    
-		    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
-		    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
-		    PrintWriter stdin = new PrintWriter(p.getOutputStream());
-		    //stdin.println("perl sendMsg.pl "+number+" \"your computer is being hacked lel\"");
-		    stdin.println("echo foo");
-		    // write any other commands you want here
-		    stdin.close();
-		    int returnCode = p.waitFor();
-		    System.out.println("Return code = " + returnCode);
-		    } catch(IOException err) {
-		    		System.out.println("IOException oh noes probably at line 43");
-		    } catch(InterruptedException errr) {
-		    	System.out.println("InterruptedException oh noes probably at line 51");
-		    }
-		 }
 
+			new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+			new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+			PrintWriter stdin = new PrintWriter(p.getOutputStream());
+			if (task == "shutdown") {
+				stdin.println("shutdown -f -s");
+			}
+			if (task == "logoff") {
+				stdin.println("logoff");
+			}
+			if (task == "sleep") {
+				stdin.println("powercfg -hibernate off");
+			}
+			// stdin.println("perl sendMsg.pl "+number+" \"your computer is
+			// being hacked lel\"");
+			stdin.println("echo foo");
+			// write any other commands you want here
+			stdin.close();
+			int returnCode = p.waitFor();
+			System.out.println("Return code = " + returnCode);
+		} catch (IOException err) {
+			System.out.println("IOException oh noes probably at line 43");
+		} catch (InterruptedException errr) {
+			System.out.println("InterruptedException oh noes probably at line 51");
+		}
 	}
 
-
+}

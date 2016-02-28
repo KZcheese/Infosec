3fb68481c369ca1a4230465063f5ecfc1f898534
Richard <richard1248c@gmail.com>
2016-02-28 09:35:48 -0500

    main class

---
+++ src/Configuration.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+
+public class Configuration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number = null;
+		try {
+			BufferedReader br = new BufferedReader("");
+		}
+	}
+
+}

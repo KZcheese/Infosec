11e7618b0100e6ffb6e186fc8d099888fc7b3b3b
John D <epicmoostudios@gmail.com>
2016-02-28 13:50:16 -0500

    Texting Scripts
    Added Text Scripts

---
+++ perl/recieveMsg.pl
@@ -0,0 +1,46 @@
+use experimental 'smartmatch';
+use Google::Voice;
+ 
+#Google login info
+my $username    = 'abblueprint2k16@gmail.com';
+my $password    = "abidealab";
+ 
+ $num_args = $#ARGV + 1;
+if ($num_args != 0) {
+    print "\nUsage: recieveMsg.pl \n";
+    exit;
+}
+
+my $filename = 'messages';
+
+#Do Not Edit Below Here!
+ 
+#create Google::Voice object and login
+
+my $gv_obj = Google::Voice->new->login($username, $password);
+ 
+ if($gv_obj == undef){
+ 
+	print "LOGIN FAILED \n";
+	exit;
+ }
+ 
+print "LOGIN SUCCESSFUL \n";
+
+open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+ 
+ foreach my $sms ( $gv_obj->sms ) {
+
+	foreach my $msg ($sms->messages){
+	
+		if($msg->outbound == "1"){
+			print $fh $msg->text, "\n";
+		}
+		
+		
+	}
+		
+
+   $sms->delete;
+}
+close $fh;

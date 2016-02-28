bdb5bb983744442c8f33b6794659c16c90228c88
Kevin Zhan <kevin.w.zhan@gmail.com>
2016-02-28 12:04:35 -0500

    Applied Fixes

--- src/main.java
+++ src/main.java
@@ -1,8 +1,23 @@
-public class main{
+import java.awt.EventQueue;
+
+public class main {
 
 	public static void main(String[] args) {
-		Configuration config = new Configuration();
-//		CommandLine cmdLine = new CommandLine();
+		/**
+		 * Launch the application.
+		 */
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DisplayGenerated frame = new DisplayGenerated();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
+
+	Configuration config = new Configuration();
+	// CommandLine cmdLine = new CommandLine();
 }

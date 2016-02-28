140b1d67da423f7d3ce606f6d909498a5b90cfd5
John D <epicmoostudios@gmail.com>
2016-02-28 17:52:28 -0500

    Finishing touches

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,7 @@
 import java.awt.EventQueue;
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
@@ -13,6 +14,7 @@ public class Main2 {
 	DisplayGenerated frame;
 	String number = "";
 	int timer = 0;
+	int gtimer = 0;
 
 	public Main2() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
 			UnsupportedLookAndFeelException, InvocationTargetException, InterruptedException {
@@ -37,28 +39,37 @@ public class Main2 {
 		// Runtime.getRuntime().exec("shutdown -f -s");
 
 		Main2 main = new Main2();
-		 main.loop();
+		main.loop();
 	}
 
 	Configuration config = new Configuration();
 
 	public void loop() throws InterruptedException {
 		while (true) {
-			after = MouseInfo.getPointerInfo().getLocation();
-			number = frame.getNumber();
-			if (!frame.muted && !before.equals(after) && timer <= 0) {
-				 PhoneIO.sendText(number, "Alert!");
-//				System.out.println("alert " + number);
-				before = after;
-				timer = frame.interval * 6;
+			if (timer <= 0) {
+				after = MouseInfo.getPointerInfo().getLocation();
+				number = frame.getNumber();
+				if (!frame.muted && !before.equals(after)) {
+					PhoneIO.sendText(number, "Alert!");
+					System.out.println("alert " + number);
+					before = after;
+					timer = frame.interval * 20;
+				}
+			}
+			if (gtimer == 0) {
+				PhoneIO.recieveText();
+				CommandLine.DoCommands(ParseTexts.parseTexts(new File("C:/Users/Admin/Desktop/texts")));
 			}
 			if (!frame.muted && frame.testBool) {
-//				System.out.println("test " + number);
-				 PhoneIO.sendText(number, "Test");
+				System.out.println("test " + number);
+				PhoneIO.sendText(number, "Test");
 				frame.testBool = false;
 			}
 			if (timer > 0)
 				timer--;
+
+			gtimer++;
+			gtimer %= 10;
 			Thread.sleep(1000 * 1);
 		}
 	}
